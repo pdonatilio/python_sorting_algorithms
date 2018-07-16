d486581cb3a565781b946eb8aae5b1945a0a9bad
pdonatilio <pdonatilio@gmail.com>
2018-07-16 13:39:39 -0300

    Insertion Sort Example

---
+++ 03_insertion_sort.py
@@ -0,0 +1,25 @@
+# Insertion Sort in Python
+# Complexity: worst and medium case: O(n2) | best case O(n)
+
+
+def insertion_sort(v):
+
+    len_v = len(v)
+
+    for i in range(1, len_v):
+        key = v[i]
+
+        j = i - 1
+        while j >= 0 and v[j] > key:
+            v[j + 1] = v[j]
+            j -= 1
+
+        v[j + 1] = key
+
+v = [4, 3, 2, 1]
+insertion_sort(v)
+print(v)
+
+v = [50,10,5,60,40,70]
+insertion_sort(v)
+print(v)
